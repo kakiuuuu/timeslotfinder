bcbc15d7bef07deda85579009f0940f22dba32cf
kakiufong <fongkakiu2001@gmail.com>
2023-04-18 13:37:20 +0800

    delete eslint

--- client/src/layouts/dashboard/nav/index.js
+++ client/src/layouts/dashboard/nav/index.js
@@ -43,7 +43,6 @@ export default function Nav({ openNav, onCloseNav }) {
     if (openNav) {
       onCloseNav();
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname]);
 
   const renderContent = (
